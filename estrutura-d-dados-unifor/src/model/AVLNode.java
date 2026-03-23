5eeb0805fa5c759deac9694457dd107c7350937c
JzBreno <jose.breno.sousa61@aluno.ifce.edu.brm>
2026-03-23 10:45:48 -0300

    adicionando metodo updateNodeJava

--- estrutura-d-dados-unifor/src/model/AVLNode.java
+++ estrutura-d-dados-unifor/src/model/AVLNode.java
@@ -1,17 +1,33 @@
 package model;
 
+import util.SHA1Util;
+
 public class AVLNode {
 
     private String word;
-    private String Hash;
+    private String hash;
     private AVLNode left;
     private AVLNode right;
     private int height;
 
     public AVLNode(String word) {
         this.word = word;
+        this.height = 1;
+        this.hash = SHA1Util.gerarHash(word);
     }
 
+    public void updateNodeData() {
+
+        int leftHeight = (left != null) ? left.getHeight() : 0;
+        int rightHeight = (right != null) ? right.getHeight() : 0;
+        this.height = 1 + Math.max(leftHeight, rightHeight);
+
+        String leftHash = (left != null) ? left.getHash() : "";
+        String rightHash = (right != null) ? right.getHash() : "";
+        String currentWordHash = SHA1Util.gerarHash(this.word);
+
+        this.hash = SHA1Util.gerarHash(leftHash + rightHash + currentWordHash);
+    }
 
     @Override
     public String toString() {
@@ -51,11 +67,11 @@ public class AVLNode {
     }
 
     public String getHash() {
-        return Hash;
+        return hash;
     }
 
     public void setHash(String hash) {
-        Hash = hash;
+        hash = hash;
     }
 
     public AVLNode getLeft() {
